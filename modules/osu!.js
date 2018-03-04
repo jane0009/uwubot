786053a8dc92ecb5c14862f9d86f7f39ecc02fc2
statefram <statefram@users.noreply.github.com>
2018-03-04 15:28:00 -0500

    debug loggings

--- modules/osu!.js
+++ modules/osu!.js
@@ -456,7 +456,7 @@ async function pushLatest(gid, cid, score, usern) {
   });
   //console.log(score);
   let map = mapL[0];
-  console.log(score);
+  console.log(score,map);
   let pp = ojsama.ppv2({
     stars: map.difficulty.rating,
     combo: parseInt(score.maxCombo),
